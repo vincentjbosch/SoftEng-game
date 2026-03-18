be087386810b1c9fb08e3d72480ea4247000fbf6
Nicki0609 <nicole.coenen@student.uva.nl>
2026-03-18 19:36:51 +0100

    player class

---
+++ player.py
@@ -0,0 +1,9 @@
+import pygame
+
+class Player():
+    def __init__(self):
+        self.slang = pygame.Surface[(8, 9), (8, 8), (8, 7)]
+        self.skin = pygame.Surface((1, 1))
+        self.skin.fill((0, 100, 0))
+        self.head = pygam,e
+
