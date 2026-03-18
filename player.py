e43cc0ea8ae011e947c04f8c846d37efd90eaf08
vincentjbosch <vincentjbosch@live.nl>
2026-03-18 18:49:36 +0100

    save before pull

--- player.py
+++ player.py
@@ -5,5 +5,5 @@ class Player():
         self.slang = pygame.Surface[(8, 9), (8, 8), (8, 7)]
         self.skin = pygame.Surface((1, 1))
         self.skin.fill((0, 100, 0))
-        self.head = pygam,e
+        self.head = pygame
 
