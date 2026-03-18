558f0c3e3db5b9c6946ae423cb1db7fd20c67c60
Nicki0609 <nicole.coenen@student.uva.nl>
2026-03-18 20:10:05 +0100

    move def

--- player.py
+++ player.py
@@ -6,6 +6,22 @@ DARKER_PURPLE = (137, 41, 133)
 class Player():
     def __init__(self):
         self.slang = [(5, 5), (4, 5), (3, 5)]
+        self.direction = "RIGHT"
+
+    def move(self):
+        head_x, head_y = self.slang[0]
+
+        if self.direction == "RIGHT":
+            new_head = (head_x + 1, head_y)
+        elif self.direction == "LEFT":
+            new_head = (head_x - 1, head_y)
+        elif self.direction == "UP":
+            new_head = (head_x, head_y - 1)
+        elif self.direction == "DOWN":
+            new_head = (head_x, head_y + 1)
+
+        self.slang.insert(0, new_head)
+        self.slang.pop
        
     def draw(self, screen, block_size):
         for i, (x, y) in enumerate(self.slang):
@@ -14,4 +30,5 @@ class Player():
             pygame.draw.rect(screen, color, rect)
             pygame.draw.rect(screen, DARKER_PURPLE, rect, 1)
 
-    
+
+
